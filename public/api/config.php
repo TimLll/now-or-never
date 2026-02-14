a146cc3a6a19803602ff63d1ca244fd38c898acb
Tim Longerich <yippiehayeah@yahoo.de>
2026-02-14 15:24:27 +0100

    Update pnpm lockfile to include new Nuxt deps

---
+++ public/api/config.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+// Copy this file to config.php (same directory) and set your RMV API key.
+// The departures endpoint will also read the RMV_API_KEY environment variable.
+const RMV_API_KEY ='6535443a-7d86-476b-af6b-ce641aed4010';
