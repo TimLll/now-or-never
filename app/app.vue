565dff2201ad93bb0dfffde68c2c788a5749f248
Tim Longerich <yippiehayeah@yahoo.de>
2026-02-17 21:55:23 +0100

    fix: cache issue browser title

--- app/app.vue
+++ app/app.vue
@@ -11,16 +11,16 @@ useHead({
   }
 })
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'Now or Never!'
+const description = 'Run or Regret.'
 
 useSeoMeta({
   title,
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  ogImage: '/assets/non-thumb.png',
+  twitterImage: '/assets/non-thumb.png',
   twitterCard: 'summary_large_image'
 })
 </script>
